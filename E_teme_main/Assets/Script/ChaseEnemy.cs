02fc4a7a80baa87372996eb085c3fa438de8431c
shotakinjou <hakumaimk-777>
2022-01-20 13:36:34 +0900

    コミット

---
+++ E_teme_main/Assets/Script/ChaseEnemy.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ChaseEnemy : MonoBehaviour
+{
+    public GameObject target;
+    private NavMeshAgent agent;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        if (target)
+        {
+            agent.destination = target.transform.position;
+        }
+    }
+}
