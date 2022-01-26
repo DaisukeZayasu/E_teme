a0f1517414f2bd501a44b4b1108b5fd846dd45a4
MSI\Dai06 <daisuke-zayasu-20h@stu.kbc.ac.jp>
2022-01-26 11:15:05 +0900

    Error correction

--- E_teme_main/Assets/whalemove.cs
+++ E_teme_main/Assets/whalemove.cs
@@ -1,49 +1,49 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-public class whalemove : MonoBehaviour
-{
-	[SerializeField] int speed;
-	// Start is called before the first frame update
-	void Start()
-	{
-	}
-	// Update is called once per frame
-	void Update()
-	{
-		//キーボードの上下キーでの操作
-		if (Input.GetKey("up"))
-		{
-			transform.position -= transform.forward * speed * Time.deltaTime;
-		}
-		if (Input.GetKey("down"))
-		{
-			transform.position += transform.forward * speed * Time.deltaTime;
-		}
-		if (Input.GetKey("right"))
-		{
-			transform.position += transform.right * speed * Time.deltaTime;
-		}
-		if (Input.GetKey("left"))
-		{
-			transform.position -= transform.right * speed * Time.deltaTime;
-		}
-		//コントローラーでの操作
-		if (Input.GetAxis("Vertical") > 0)
-		{
-			transform.position += transform.forward * speed * Time.deltaTime;
-		}
-		if (Input.GetAxis("Vertical") < 0)
-		{
-			transform.position -= transform.forward * speed * Time.deltaTime;
-		}
-		if (Input.GetAxis("Horizontal") > 0)
-		{
-			transform.position += transform.right * speed * Time.deltaTime;
-		}
-		if (Input.GetAxis("Horizontal") < 0)
-		{
-			transform.position -= transform.right * speed * Time.deltaTime;
-		}
-	}
-}
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//public class whalemove : MonoBehaviour
+//{
+//	[SerializeField] int speed;
+//	// Start is called before the first frame update
+//	void Start()
+//	{
+//	}
+//	// Update is called once per frame
+//	void Update()
+//	{
+//		//キーボードの上下キーでの操作
+//		if (Input.GetKey("up"))
+//		{
+//			transform.position -= transform.forward * speed * Time.deltaTime;
+//		}
+//		if (Input.GetKey("down"))
+//		{
+//			transform.position += transform.forward * speed * Time.deltaTime;
+//		}
+//		if (Input.GetKey("right"))
+//		{
+//			transform.position += transform.right * speed * Time.deltaTime;
+//		}
+//		if (Input.GetKey("left"))
+//		{
+//			transform.position -= transform.right * speed * Time.deltaTime;
+//		}
+//		//コントローラーでの操作
+//		if (Input.GetAxis("Vertical") > 0)
+//		{
+//			transform.position += transform.forward * speed * Time.deltaTime;
+//		}
+//		if (Input.GetAxis("Vertical") < 0)
+//		{
+//			transform.position -= transform.forward * speed * Time.deltaTime;
+//		}
+//		if (Input.GetAxis("Horizontal") > 0)
+//		{
+//			transform.position += transform.right * speed * Time.deltaTime;
+//		}
+//		if (Input.GetAxis("Horizontal") < 0)
+//		{
+//			transform.position -= transform.right * speed * Time.deltaTime;
+//		}
+//	}
+//}
