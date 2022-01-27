7a9e73b952ca0fa56d4c8bb09345ba688d43f088
MSI\Dai06 <daisuke-zayasu-20h@stu.kbc.ac.jp>
2022-01-27 15:49:52 +0900

    クリア表示作成

---
+++ E_teme_main/Assets/Goal.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Goal : MonoBehaviour
+{
+    public GameObject sea;
+    public GameObject Clear;
+
+    private void Start()
+    {
+        Clear.SetActive(false);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == sea.name)
+        {
+            Clear.GetComponent<Text>();
+            Clear.SetActive(true);
+        }
+    }
+}
